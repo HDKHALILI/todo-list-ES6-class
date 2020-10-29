4c51fa7d4494df4dbccfcd669a9cdac959e40107
HDKHALILI <14176079+HDKHALILI@users.noreply.github.com>
2020-10-30 09:40:21 +1100

    add more methods to TodoList class

--- todolist.js
+++ todolist.js
@@ -31,3 +31,157 @@ class Todo {
     return this.title;
   }
 }
+
+// This class represents a collection of Todo objects.
+// You can perform typical collection-oriented actions
+// on a TodoList object, including iteration and selection.
+
+class TodoList {
+  constructor(title) {
+    this.title = title;
+    this.todos = [];
+  }
+
+  add(todo) {
+    if (todo instanceof Todo) {
+      this.todos.push(todo);
+    } else {
+      throw new TypeError("can only add Todo objects");
+    }
+  }
+
+  size() {
+    return this.todos.length;
+  }
+
+  first() {
+    return this.todos[0];
+  }
+
+  last() {
+    return this.todos[this.size() - 1];
+  }
+
+  itemAt(index) {
+    this._validateIndex(index);
+    return this.todos[index];
+  }
+
+  markDoneAt(index) {
+    this.itemAt(index).markDone();
+  }
+
+  markUndoneAt(index) {
+    this.itemAt(index).markUndone();
+  }
+
+  isDone() {
+    return this.todos.every(todo => todo.isDone());
+  }
+
+  shift() {
+    return this.todos.shift();
+  }
+
+  pop() {
+    return this.todos.pop();
+  }
+
+  removeAt(index) {
+    this._validateIndex(index);
+    return this.todos.splice(index, 1)[0];
+  }
+
+  toString() {
+    const title = `---- ${this.title} ----`;
+    const list = this.todos.map(todo => todo.toString()).join("\n");
+    return `${title}\n${list}`;
+  }
+
+  forEach(callback) {
+    this.todos.forEach(callback);
+  }
+
+  filter(callback) {
+    const list = new TodoList(this.title);
+    this.forEach(todo => {
+      if (callback(todo)) {
+        list.add(todo);
+      }
+    });
+
+    return list;
+  }
+
+  findByTitle(title) {
+    return this.filter(
+      todo => todo.getTitle().toLowerCase() === title.toLowerCase()
+    ).first();
+  }
+
+  allDone() {
+    return this.filter(todo => todo.isDone());
+  }
+
+  allNotDone() {
+    return this.filter(todo => !todo.isDone());
+  }
+
+  markDone(title) {
+    const todo = this.findByTitle(title);
+    if (todo) {
+      todo.markDone();
+    }
+  }
+
+  markAllDone() {
+    this.forEach(todo => todo.markDone());
+  }
+
+  markAllUndone() {
+    this.forEach(todo => todo.markUndone());
+  }
+
+  toArray() {
+    return this.todos.slice();
+  }
+
+  _validateIndex(index) {
+    if (!(index in this.todos)) {
+      throw new ReferenceError(`invalid index: ${index}`);
+    }
+  }
+}
+
+const list = new TodoList("Today's Todos");
+// Omitted code
+
+let todo1 = new Todo("Buy milk");
+let todo2 = new Todo("Clean room");
+let todo3 = new Todo("Go to the gym");
+let todo4 = new Todo("Go shopping");
+
+list.add(todo1);
+list.add(todo2);
+list.add(todo3);
+list.add(todo4);
+
+let todo5 = new Todo("Feed the cats");
+let todo6 = new Todo("Study for Launch School");
+list.add(todo5);
+list.add(todo6);
+// todo1.markDone();
+list.markDone("buy milk");
+// todo5.markDone();
+list.markAllDone();
+
+// list.forEach(todo => console.log(todo.toString()));
+// const doneTodos = list.filter(todo => todo.isDone());
+// console.log(doneTodos.first());
+// console.log(list.findByTitle("buy milk"));
+// console.log(list.findByTitle("buy mil"));
+
+console.log(list.allDone());
+// list.markAllUndone();
+// console.log(list.allNotDone());
+// console.log(list.toArray());
